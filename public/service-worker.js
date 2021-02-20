6bd1bcd5345bf0c492fe8211eb035d255cf951bb
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-02-20 13:19:58 -0700

    finalize service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,14 +5,19 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "/",
-  // "/server.js",
-  // "/models/transaction.js",
   "/css/styles.css",
   "/js/idb.js",
   "/js/index.js",
   "/index.html",
   "/manifest.json",
-  // "/routes/api.js"
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png"
 ];
 
 self.addEventListener('install', function (e) {
@@ -24,36 +29,36 @@ self.addEventListener('install', function (e) {
   )
 })
 
-// self.addEventListener('activate', function (e) {
-//   e.waitUntil(
-//     caches.keys().then(function (keyList) {
-//       let cacheKeeplist = keyList.filter(function (key) {
-//         return key.indexOf(APP_PREFIX);
-//       });
-//       cacheKeeplist.push(CACHE_NAME);
-//       return Promise.all(
-//         keyList.map(function (key, i) {
-//           if (cacheKeeplist.indexOf(key) === -1) {
-//             console.log('deleting cache : ' + keyList[i]);
-//             return caches.delete(keyList[i]);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
 
-// self.addEventListener('fetch', function (e) {
-//   console.log('fetch request : ' + e.request.url)
-//   e.respondWith(
-//     caches.match(e.request).then(function (request) {
-//       if (request) { // if cache is available, respond with cache
-//         console.log('responding with cache : ' + e.request.url)
-//         return request
-//       } else {       // if there are no cache, try fetching request
-//         console.log('file is not cached, fetching : ' + e.request.url)
-//         return fetch(e.request)
-//       }
-//     })
-//   )
-// })
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+});
